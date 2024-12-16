5df132b77e5c5df1d8f031b2275dfe27855fa95f
Anton <80984465+Antongo22@users.noreply.github.com>
2024-12-16 16:50:01 +0300

    добавлены стили

--- TrapsGame/Windows/MainWindow.xaml.cs
+++ TrapsGame/Windows/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TrapsGame
+namespace TrapsGame.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
